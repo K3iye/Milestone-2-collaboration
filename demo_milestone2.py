c84ee4070be896ef42bde080f93b4542bf87eb0c
Ryan Nguyen <ryanwin@Ryans-MacBook-Pro.local>
2026-04-08 22:46:11 -0400

    missed typo

--- demo_milestone2.py
+++ demo_milestone2.py
@@ -13,7 +13,7 @@ course.request_enroll(stu1, "2026-04-08")
 course.request_enroll(stu2, "2026-04-02")
 course.request_enroll(stu3, "2026-04-05") # goes to waitlist capacity(2)
 
-print("EnrolledI:", course.enrolled)
+print("Enrolled:", course.enrolled)
 print("Waitlist size:", len(course.waitlist))
 
 course.sort_enrolled("id", "insertion")
